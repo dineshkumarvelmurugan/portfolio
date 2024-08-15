75044ce725b760e3ee4d61c81a650bf465ccd685
dineshkumarvelmurugan <veldineshkumar@gmail.com>
2024-08-15 11:41:55 +0530

    Initial Commit

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      
+    </Row>
+  );
+}
+
+export default Toolstack;
---
+++ src/components/contact/contact.js
@@ -0,0 +1,70 @@
+// src/components/Contact.js
+
+import React, { useState } from 'react';
+import './cont.css';
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="contact-container">
+      <h1>Contact Me</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            placeholder='your '
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            placeholder='your '
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            placeholder='your '
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
