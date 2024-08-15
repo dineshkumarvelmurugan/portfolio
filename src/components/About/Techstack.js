befafdbf79a55999a6b5cefae94a89b24459cbcc
dineshkumarvelmurugan <veldineshkumar@gmail.com>
2024-08-15 11:48:02 +0530

    .

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -13,7 +13,7 @@ import {
 
 function Techstack() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }} >
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
       </Col>
